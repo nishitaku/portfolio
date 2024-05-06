7070ad9c997fb3b0e937c8463eaded8eff3ffa4f
Takuro Nishino <29690066+nishitaku@users.noreply.github.com>
2024-05-06 14:07:01 +0900

    feat: update astro v3 (#14)
    * feat: npm install astro@3
    
    * feat: Replaced @astrojs/image/client with astro/client
    
    * feat: Removed deprecated @astrojs/image
    
    * feat: Replaced @astrojs/image/components with astro:assets
    
    * fix: image path
    
    * feat: fix import.meta.env.BASE_URL trailingSlash changes

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,20 +1,11 @@
-import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
-import { defineConfig } from 'astro/config';
-
-// https://astro.build/config
-import image from "@astrojs/image";
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://nishitaku.github.io',
-  base: '/portfolio',
-  integrations: [mdx(), sitemap(), tailwind(), image(
-    {
-      serviceEntryPoint: '@astrojs/image/sharp',
-      cacheDir: "./.cache/image",
-      logLevel: 'debug',
-    }
-  )]
+  site: "https://nishitaku.github.io",
+  base: "portfolio/",
+  integrations: [mdx(), sitemap(), tailwind()],
 });
