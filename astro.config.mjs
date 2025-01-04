fd54495bf90572769c56ec38d4cc4330a3d9ca6c
nishitaku <29690066+nishitaku@users.noreply.github.com>
2025-01-05 00:38:30 +0900

    build: exec format

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,13 @@
-import mdx from "@astrojs/mdx";
-import partytown from "@astrojs/partytown";
-import sitemap from "@astrojs/sitemap";
-import tailwind from "@astrojs/tailwind";
-import { defineConfig } from "astro/config";
+import mdx from '@astrojs/mdx';
+import partytown from '@astrojs/partytown';
+import sitemap from '@astrojs/sitemap';
+import tailwind from '@astrojs/tailwind';
+import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://nishitaku.github.io",
-  base: "portfolio/",
+  site: 'https://nishitaku.github.io',
+  base: 'portfolio/',
   integrations: [
     mdx(),
     sitemap(),
@@ -15,7 +15,7 @@ export default defineConfig({
     partytown({
       // Adds dataLayer.push as a forwarding-event.
       config: {
-        forward: ["dataLayer.push"],
+        forward: ['dataLayer.push'],
       },
     }),
   ],
