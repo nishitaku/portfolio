9cdfbf9ac930f27b76e846fe8abc6352c441faab
nishitaku <29690066+nishitaku@users.noreply.github.com>
2025-06-08 17:44:07 +0900

    build: pnpm astro add astro-icon

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,7 @@ import mdx from '@astrojs/mdx';
 import partytown from '@astrojs/partytown';
 import sitemap from '@astrojs/sitemap';
 import tailwind from '@astrojs/tailwind';
+import icon from 'astro-icon';
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
@@ -18,5 +19,6 @@ export default defineConfig({
         forward: ['dataLayer.push'],
       },
     }),
+    icon(),
   ],
 });
